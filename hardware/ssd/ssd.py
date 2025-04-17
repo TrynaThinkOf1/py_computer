1c5248ea9d5a232f9070829c5f461146732fae0e
TrynaThinkOf1 <zeviberlin@gmail.com>
2025-04-17 18:37:53 -0400

    figuring out SSD stuff

--- hardware/ssd/ssd.py
+++ hardware/ssd/ssd.py
@@ -1,28 +1,64 @@
 import os
-import aiofiles
+#import aiofiles
 from typing import Optional
 
 import ssd_errors
 
 class SSD:
-    def __init__(self, device_name: Optional[str], storage_size: int = 1000000) -> None:
+    def __init__(self, device_name: Optional[str], max_storage_size: int = 1000000) -> None:
         """
         Create an SSD Object for storing long-term data in a filesystem.
 
         :param device_name: The name of THIS SSD device, that will be the base directory for the file system
-        :param storage_size: How many bytes this SSD will store, default is 1000000 (1MB)
+        :param max_storage_size: How many bytes this SSD will store, default is 1000000 (1MB)
         """
         self.device_name = device_name
-        self.storage: os.path = os.path.join("./storage", storage_dir_name)
-        self.storage_size: int = storage_size
+        self.storage_path: os.path = os.path.join("./storage", storage_dir_name)
+        self.max_storage_size: int = max_storage_size
         self.currently_storing_size: int = 0
 
-    def change_storage_size(self, new_storage_size: int) -> None:
+        if not os.path.exists(self.storage_path):
+            os.makedirs(self.storage_path)
+        else:
+            error_msg: str = f"Cannot create SSD {device_name} because that space is allocated to another SSD.\n\t\tTry changing the device name."
+            del self
+            raise ssd_errors.DirectoryAlreadyExistsError(error_msg)
+
+    def __str__(self):
+        return f"SSD Device {self.device_name} with storage size {self.max_storage_size}, currently {(self.currently_storing_size / self.max_storage_size) * 100}% full"
+    def __repr__(self):
+        return self.__str__()
+    def __bool__(self):
+        return self.currently_storing_size < self.max_storage_size
+    def __del__(self):
+        os.remove(self.storage_path)
+
+    def change_max_storage_size(self, new_max_storage_size: int) -> None:
         """
         Change the amount of storage this SSD can have
 
-        :param new_storage_size: The new storage bytes amount
+        :param new_max_storage_size: The new storage bytes amount
         :return:
         """
-        if new_storage_size < self.currently_storing_size:
-            raise ssd_errors.StorageFullError(device_name=self.device_name, storage_size=self.currently_storing_size)
+        if new_max_storage_size < self.currently_storing_size:
+            error_msg: str = f"Cannot compress storage from {self.max_storage_size} bytes to {new_max_storage_size} bytes on SSD {self.device_name} because it is currently storing {self.currently_storing_size} bytes."
+            raise ssd_errors.StorageFullError(message=error_msg)
+        else:
+            self.max_storage_size = new_max_storage_size
+
+    def create_directory(self, parent_path: str, dir_name: str) -> None:
+        """
+        Create a directory inside this SSD's filesystem
+        :param parent_path:
+        :param dir_name:
+        :return:
+        """
+
+if __name__ == "__main__":
+    storage1 = SSD(device_name="storage1", max_storage_size=256000000000)
+    print(storage1)
+    print(storage1.__bool__())
+    print(storage1.max_storage_size)
+    print(storage1.currently_storing_size)
+    print(storage1.storage_path)
+    del storage1
--- hardware/ssd/ssd_errors.py
+++ hardware/ssd/ssd_errors.py
@@ -1,3 +1,19 @@
 class StorageFullError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
+
+class DirectoryAlreadyExistsError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+class DirectoryNotFoundError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+class FileAlreadyExistsError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+class FileNotFoundError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
